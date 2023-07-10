bafd172c65812c24cdf4f7e97e72bb9c39609b2c
bjorkrothjm <64375352+bjorkrothjm@users.noreply.github.com>
2023-07-10 12:56:57 +0000

    Pending changes exported from your codespace

--- .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
+++ .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
@@ -7,14 +7,6 @@ const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
  * 1) Create the mapping function and use it to generate a new array of
  * planets in html called planetsInHTML
  */
-const planetList = plants.map((planet, index) => {
-	return <li class="list-group-item">planet</li>
-	
-
-	
-});
-
-
 
 
 
